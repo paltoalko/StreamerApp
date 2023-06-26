d4299474aaefdc28770ee43a0699f9ffedb08141
paltoalko <martas90909@me.com>
2023-06-26 15:43:11 +0200

    streamform init

---
+++ frontend/src/components/form/StreamerForm.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
+import { Typography, Box, Divider, CircularProgress } from '@mui/material';
+const StreamerForm: React.FC<object> = () => {
+  return (
+    <Box>
+      <Typography>Form</Typography>
+    </Box>
+  );
+};
+
+export default StreamerForm;
--- frontend/src/components/pages/Main.tsx
+++ frontend/src/components/pages/Main.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
 // import { v4 as uuid } from 'uuid';
-import { Typography, Box, Icon } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import styles from '../../assets/styles/Main.module.css';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
+import StreamerForm from 'components/form/StreamerForm';
 const Main: React.FC<object> = () => {
   return (
-    <Box className={styles.logo}>
-      <WhatshotIcon sx={{ fontSize: '42px' }} color="secondary" />
+    <Box>
+      <Box className={styles.logo}>
+        <WhatshotIcon sx={{ fontSize: '42px' }} color="secondary" />
 
-      <Typography
-        color="primary"
-        fontSize={42}
-        fontWeight={300}
-        lineHeight={1}
-        fontStyle="italic"
-      >
-        STREAMER
-      </Typography>
-      <Typography
-        color="secondary"
-        fontSize={30}
-        fontStyle="italic"
-        fontWeight={700}
-        lineHeight={1}
-      >
-        APP
-      </Typography>
+        <Typography
+          color="primary"
+          fontSize={42}
+          fontWeight={300}
+          lineHeight={1}
+          fontStyle="italic"
+        >
+          STREAMER
+        </Typography>
+        <Typography
+          color="secondary"
+          fontSize={30}
+          fontStyle="italic"
+          fontWeight={700}
+          lineHeight={1}
+        >
+          APP
+        </Typography>
+      </Box>
+      <StreamerForm />
     </Box>
   );
 };
