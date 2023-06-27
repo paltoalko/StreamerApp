0b23445c26c86cdfda3937d407dc14c78830fad3
paltoalko <martas90909@me.com>
2023-06-27 16:28:23 +0200

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
 import theme from './assets/theme';
 import Main from './components/pages/Main';
+import StreamerPage from './components/pages/StreamerPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Main />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/streamers/:id" element={<StreamerPage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
